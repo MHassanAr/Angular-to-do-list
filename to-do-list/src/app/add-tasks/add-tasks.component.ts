5dc263a504ef6787799c2eb032774d680b2769be
MHassanAr <mhassana2344@gmail.com>
2024-10-26 17:52:19 +0500

    Import view-task (Child Component) and write the login of add task and delete task

--- to-do-list/src/app/add-tasks/add-tasks.component.ts
+++ to-do-list/src/app/add-tasks/add-tasks.component.ts
@@ -1,12 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ViewTasksComponent } from "../view-tasks/view-tasks.component";
 
 @Component({
   selector: 'app-add-tasks',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, ViewTasksComponent],
   templateUrl: './add-tasks.component.html',
-  styleUrl: './add-tasks.component.css'
+  styleUrls: ['./add-tasks.component.css'], // Corrected 'styleUrl' to 'styleUrls'
 })
 export class AddTasksComponent {
+  tasksArray: { task: string; id: number }[] = [];
+  currentTask = '';
+  nextId: number = 1;
 
+  addTask() {
+    if (this.currentTask) {
+      this.tasksArray.push({ task: this.currentTask, id: this.nextId++ }); // Correctly add task
+      this.currentTask = ''; // Reset after adding
+    }
+  }
+
+  deleteTask(id: number) {
+    this.tasksArray = this.tasksArray.filter((task) => task.id !== id);
+  }
 }
