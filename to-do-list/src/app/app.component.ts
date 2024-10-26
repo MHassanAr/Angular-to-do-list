55e69d1789b32c39da32e621134046069d557b83
MHassanAr <mhassana2344@gmail.com>
2024-10-26 17:49:18 +0500

    Import add-task component

--- to-do-list/src/app/app.component.ts
+++ to-do-list/src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AddTasksComponent } from './add-tasks/add-tasks.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AddTasksComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  
-}
+export class AppComponent {}
