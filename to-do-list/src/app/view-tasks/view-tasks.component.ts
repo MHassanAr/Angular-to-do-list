5d43ec9476bdc8c2cce242440abbaa8d890fa838
MHassanAr <mhassana2344@gmail.com>
2024-10-26 17:55:59 +0500

    Build login to send and recieve data to and from parent component

--- to-do-list/src/app/view-tasks/view-tasks.component.ts
+++ to-do-list/src/app/view-tasks/view-tasks.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-view-tasks',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './view-tasks.component.html',
-  styleUrl: './view-tasks.component.css'
+  styleUrl: './view-tasks.component.css',
 })
 export class ViewTasksComponent {
+  @Input() viewTask: { task: string; id: number } | null = null;
+  @Output() taskDelete = new EventEmitter<number>();
 
+  deleteTask() {
+    if (this.viewTask) {
+      this.taskDelete.emit(this.viewTask.id);
+    }
+  }
 }
